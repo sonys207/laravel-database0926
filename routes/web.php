e3c11eb69adf9f4d5517573e24df7c4c818e090c
Jichen Liu <jichen.liu85@gmail.com>
2021-09-26 22:52:31 -0400

    0926-first version

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+  return view('welcome');
+})->name('pizza.root');
+
+// pizza routes
+Route::get('/pizzas', 'App\Http\Controllers\PizzaController@index');
+Route::get('/pizzas/create', 'App\Http\Controllers\PizzaController@create');
+Route::post('/pizzas', 'App\Http\Controllers\PizzaController@store');
+Route::get('/pizzas/{id}', 'App\Http\Controllers\PizzaController@show');
+Route::delete('/pizzas/{id}', 'App\Http\Controllers\pizzaController@destroy');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
