5221ea90179181ff0cbc41432d6d95ffe7a7878e
Jichen Liu <jichen.liu85@gmail.com>
2021-09-28 15:53:58 -0400

    add auth

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 })->name('pizza.root');
 
 // pizza routes
-Route::get('/pizzas', 'App\Http\Controllers\PizzaController@index');
+Route::get('/pizzas', 'App\Http\Controllers\PizzaController@index')->middleware('auth');
 Route::get('/pizzas/create', 'App\Http\Controllers\PizzaController@create');
 Route::post('/pizzas', 'App\Http\Controllers\PizzaController@store');
 Route::get('/pizzas/{id}', 'App\Http\Controllers\PizzaController@show');
